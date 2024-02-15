4f90612c7bc2db6d737afb6fbd937ef01932a8f7
CaptainPolyana <Danchik557@mail.ru>
2024-02-15 20:44:01 +0300

    ProjectWithNumbers

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,9 @@ class HomePage extends StatelessWidget {
                           height: double.maxFinite,
                           margin: EdgeInsets.only(bottom: 8, left: 8),
                           color: Colors.redAccent,
+                          alignment: Alignment.center,
+                          child: Text('1', style: TextStyle(fontSize: 30, color: Colors.black),
+                          ),
                         )
                     ),
 
@@ -48,6 +51,9 @@ class HomePage extends StatelessWidget {
                           height: double.maxFinite,
                           margin: EdgeInsets.only(bottom: 8, left: 8),
                           color: Colors.brown,
+                          alignment: Alignment.center,
+                          child: Text('3', style: TextStyle(fontSize: 30, color: Colors.black),
+                          ),
                         )
                     ),
                     Expanded( flex: 1,
@@ -56,6 +62,9 @@ class HomePage extends StatelessWidget {
                           height: double.maxFinite,
                           margin: EdgeInsets.only(bottom: 8, left: 8),
                           color: Colors.blue[900],
+                          alignment: Alignment.center,
+                          child: Text('5', style: TextStyle(fontSize: 30, color: Colors.black),
+                          ),
                         )
                     ),
                     Expanded( flex: 4,
@@ -65,6 +74,9 @@ class HomePage extends StatelessWidget {
                           height: double.maxFinite,
                           margin: EdgeInsets.only(bottom: 8, left: 8),
                           color: Colors.black,
+                          alignment: Alignment.center,
+                          child: Text('7', style: TextStyle(fontSize: 30, color: Colors.white),
+                          ),
                         )
                     ),
                   ],
@@ -81,6 +93,9 @@ class HomePage extends StatelessWidget {
                             height: double.maxFinite,
                             margin: EdgeInsets.only(bottom: 8, left: 8, right: 8),
                             color: Colors.purple,
+                            alignment: Alignment.center,
+                            child: Text('2', style: TextStyle(fontSize: 30, color: Colors.black),
+                            ),
                           )
                       ),
 
@@ -90,6 +105,9 @@ class HomePage extends StatelessWidget {
                             height: double.maxFinite,
                             margin: EdgeInsets.only(bottom: 8, left: 8, right: 8),
                             color: Colors.indigo[700],
+                            alignment: Alignment.center,
+                            child: Text('4', style: TextStyle(fontSize: 30, color: Colors.black),
+                            ),
                           )
                       ),
                       Expanded( flex: 1,
@@ -98,6 +116,9 @@ class HomePage extends StatelessWidget {
                             height: double.maxFinite,
                             margin: EdgeInsets.only(bottom: 8, left: 8, right: 8),
                             color: Colors.orange,
+                            alignment: Alignment.center,
+                            child: Text('6', style: TextStyle(fontSize: 30, color: Colors.black),
+                            ),
                           )
                       ),
                       Expanded( flex: 4,
@@ -107,6 +128,9 @@ class HomePage extends StatelessWidget {
                             height: double.maxFinite,
                             margin: EdgeInsets.only(bottom: 8, left: 8, right: 8),
                             color: Colors.lime[900],
+                            alignment: Alignment.center,
+                            child: Text('8', style: TextStyle(fontSize: 30, color: Colors.black),
+                            ),
                           )
                       ),
                     ]
